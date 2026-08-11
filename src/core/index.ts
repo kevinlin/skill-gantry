e0a5e7d8161c9ba133509862e4a9c4755343366e
Kevin Lin <kevin.lin@zuhlke.com>
2026-08-11 10:27:44 +0800

    feat(upgrade): parse CHANGELOG.md and slice the entries above a version

--- src/core/index.ts
+++ src/core/index.ts
@@ -202,6 +202,7 @@ export {
   type SuppressionPreview,
   type SuppressionRequest,
 } from './suppress/target.js'
+export { entriesAbove, parseChangelog } from './upgrade/changelog.js'
 export { loadUpgradeState, saveUpgradeState } from './upgrade/state.js'
 export type {
   ChangelogEntry,
